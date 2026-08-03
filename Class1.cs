b232b197105ebdc86d9710340a747b94eec08a17
Ayushi💕 <ayushithummar14@gmail.com>
2026-08-03 17:45:53 +0530

    Basic pattern programs

--- Class1.cs
+++ Class1.cs
@@ -32,20 +32,72 @@ namespace ConsoleApp1
             //    Console.WriteLine();
             //}
 
-            int n = 15;
-            for(int i=1; i<=5; i++)
-            {
-                for(int j=1; j<=n-j+i; j++)
-                {
-                    Console.Write(n);
-                    if (n == 1)
-                    {
-                        break;
-                    }
-                    n--;
-                }
-                Console.WriteLine();
-            }
-         }
+
+            //int n = 15;
+            //for(int i=1; i<=5; i++)
+            //{
+            //    for(int j=1; j<=n-j+i; j++)
+            //    {
+            //        Console.Write(n);
+            //        if (n == 1)
+            //        {
+            //            break;
+            //        }
+            //        n--;
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            //int n = 5;
+            //for(int i=1; i<=n; i++)
+            //{
+            //    for(int j=1; j<=i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            //int n = 5;
+            //for(int i=1; i<=n; i++)
+            //{
+            //    for(int j=1; j<=n-i+1; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            //int n = 5;
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    for (int s = 1; s <= i-1; s++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int j = 1; j <= n-i+1; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //int n = 5;
+            //for(int i=1; i<=n; i++)
+            //{
+            //    for(int s=1; s<=n-i; s++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for(int j=1; j<=i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+        }
     }
 }
