87f80a7ef8525c830fe8dc22186fa7217eb98cc3
Ayushi💕 <ayushithummar14@gmail.com>
2026-08-05 15:36:34 +0530

    Method overloading & inheritance

---
+++ Inheritance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Inheritance
+    {
+        public class Chocolate
+        {
+            public string Taste = "Sweet";
+            public void Price()
+            {
+                Console.WriteLine("Price of chocolate is 100.");
+            }
+        }
+        public class DarkChocolate : Chocolate
+        {
+            public string name = "Amul";
+        }
+    }
+}
---
+++ MethodOverloading.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class MethodOverloading
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Addition(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+        public static double Addition(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
